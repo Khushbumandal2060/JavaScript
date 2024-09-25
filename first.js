fe5fcf02f4619c02a3471b20be0f3b5aadcf4185
khushi <khusbumandal2060@gmail.com>
2024-09-25 23:59:47 +0545

    Command to print JS code

--- first.js
+++ first.js
@@ -2,7 +2,6 @@ console.log("Khushbu mandal");
 console.log("I love to protect you");
 console.log(22);
 console.log("Gola");
-fullName= "Chandani Mandal";
 age = 14;
 price = 25.02;
 x = null;
@@ -12,3 +11,4 @@ console.log(price);
 console.log(x);
 console.log(y);
 console.log(isFollow);
+
