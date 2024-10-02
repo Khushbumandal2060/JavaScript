fa40716a37954926cd782c728c1efb7cae7a1cb3
khushi <khusbumandal2060@gmail.com>
2024-10-02 23:13:58 +0545

    Comparison of Operators in JS

--- assign_operator.js
+++ assign_operator.js
@@ -1,11 +1,8 @@
 
 //Asignment Operators
 let a = 5;
-/let b = 2;
+let b = 2;
 
 a**= 4;   // a = a **4
 console.log("a = ", a); //1  
 
-//Comparison Operator
-console.log("5 == 5", a =b); 
- 
---
+++ compare_operator.js
@@ -0,0 +1,6 @@
+//Comparison Operator
+let a = 5;  //number
+let b = "5";  //string  -> number
+
+console.log("a == b", a = b);  //true
+ 
