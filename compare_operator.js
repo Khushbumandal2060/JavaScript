b04651986ddd39694d5030e7f073aa94698ce54d
khushi <khusbumandal2060@gmail.com>
2024-10-02 23:53:11 +0545

    Comparison of operators in js

--- compare_operator.js
+++ compare_operator.js
@@ -1,6 +1,17 @@
 //Comparison Operator
-let a = 5;  //number
+let a = 6;  //number
 let b = "5";  //string  -> number
 
-console.log("a == b", a == b);  //true
+console.log("a >= b", a >= b);  //true
+
+
+
+
+//logical Operator
+let a = 6;
+let b = 5;
+
+let cond1 = a > b;  //true
+let cond2 = a == 5; //true
+console.log("cond1 && cond2 =", cond1 && cond2);
  
