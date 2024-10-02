cad5c39ed6447cb922abf0778faf022b83104cac
khushi <khusbumandal2060@gmail.com>
2024-10-02 23:55:42 +0545

    Logical Operator in js

--- compare_operator.js
+++ compare_operator.js
@@ -7,11 +7,3 @@ console.log("a >= b", a >= b);  //true
 
 
 
-//logical Operator
-let a = 6;
-let b = 5;
-
-let cond1 = a > b;  //true
-let cond2 = a == 5; //true
-console.log("cond1 && cond2 =", cond1 && cond2);
- 
