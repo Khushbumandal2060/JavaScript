f64df33f55e0d1db0dddc7f8c9b6956ce85c7c49
khushi <khusbumandal2060@gmail.com>
2024-10-03 23:42:27 +0545

    Cannot vote

---
+++ conditional.js
@@ -0,0 +1,13 @@
+//Conditional Statement
+
+let age = 16;
+let mode = "dark";
+let color;
+
+if (age >= 18) {
+    console.log("you can vote");
+}
+
+if(age < 18) {
+    console.log("you CANNOT vote");
+}
