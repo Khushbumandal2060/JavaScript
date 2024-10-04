32162376a75317210f60c450a693e0646c6f3c37
khushi <khusbumandal2060@gmail.com>
2024-10-04 23:51:49 +0545

    Ternary Operator in JSS

--- ternary_operator.js
+++ ternary_operator.js
@@ -3,5 +3,5 @@
 //Ternary Operator
 
 let age = 15;
-let result = age >= 18 ? "adult" : "not adult";
+let result = age >= 18 ? "adult" : "not adult";  //Simpler or compact if_else condition
 console.log(result);
